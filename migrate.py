4f874f72530503f21d0a85e6d06eaa15b98efa1b
cristian_mercadante <crimerca96@gmail.com>
2019-07-24 19:03:42 +0200

    delete old project

--- migrate.py
+++
@@ -1,9 +0,0 @@
-from grouppy import app, db
-
-if __name__ == '__main__':
-    from flask_script import Manager
-    from flask_migrate import Migrate, MigrateCommand
-    migrate = Migrate(app, db)
-    manager = Manager(app)
-    manager.add_command('db', MigrateCommand)
-    manager.run()
